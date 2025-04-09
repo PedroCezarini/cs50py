0775d14b35728b7d16e6c43ac61181d4febc917c
Pedro Cezarini <pedrocezarini13@gmail.com>
2025-04-09 19:53:43 -0300

    começando exercicio bank

---
+++ week1/bank.py
@@ -0,0 +1,23 @@
+#In a file called bank.py, implement a program that prompts the user for a greeting. 
+
+#If the greeting starts with “hello”, output $0.
+#If the greeting starts with an “h” (but not “hello”), output $20. 
+#Otherwise, output $100.
+#Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+
+txt = input("Greetings! Now enter your greetings: ").lower()
+x = txt.split(" ")
+
+
+
+#if primeira palavra for hello
+    #print "$0"
+
+#elif se primeira letra é h
+    #print "$20"
+
+#else
+    #print "$100"
+
+
+
