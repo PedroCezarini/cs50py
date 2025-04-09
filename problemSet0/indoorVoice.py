a4609b033e626feb73c362af16b9a13edc68afc3
Pedro Cezarini <pedrocezarini13@gmail.com>
2025-04-09 17:10:41 -0300

    resoluçao exercicio indoor voice

---
+++ problemSet0/indoorVoice.py
@@ -0,0 +1,5 @@
+print("Enter a phrase in CAPS LOCK to be converted to lower case here: ")
+#usando casefold pra transformar todo o input em letra minuscula
+userphrase = input().casefold()
+
+print(userphrase)
