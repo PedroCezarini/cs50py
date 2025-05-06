568811d35490eeeb410528ae85ce6d00f345a39f
Pedro Cezarini <135235828+PedroCezarini@users.noreply.github.com>
2025-05-06 12:14:17 -0300

    Create coke.py

---
+++ week2/coke.py
@@ -0,0 +1,7 @@
+while amount_due != 0: #while amount due is greater than 0
+
+(int(amount_due)) = 50 #starting amount_due var
+
+(int(inserted_coin)) = input("Insert coin: ") #getting the value of the inserted coin
+
+amount_due -= inserted_coin #getting new value for amount_due
